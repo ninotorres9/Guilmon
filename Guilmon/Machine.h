4c7f49553fe62d50a6c92a30b4c95a8aacf60da3
zimu <delongdal@outlook.com>
2018-03-06 14:00:32 +0800

    修改_2：完成Scope类（但未实用）

--- Guilmon/Machine.h
+++ Guilmon/Machine.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <map>
 #include <iostream>
+#include <memory>
 
 #include "Token.h"
 
@@ -43,11 +44,6 @@ namespace Guilmon {
 
 	class LocalScope {
 	public:
-		/*
-		create
-		set
-		find
-		*/
 		inline void createVariable(const std::string& name, Value* value) {
 			variableTable_.insert({ name, value });
 		}
@@ -61,13 +57,94 @@ namespace Guilmon {
 			auto ptr = variableTable_.find(name);
 			variableTable_.erase(ptr);
 		}
-	private:
+	public:
 		std::map<std::string, Value*> variableTable_;
 	};
 
-	class GlobalScope {
+	/*
+		new Scope
+		开一个for循环
+		scope.inScope() -> 创建并跳转
+		create
+		find
+		set
+		delete
+		scope.outScope() -> 删除并退出
+		
+	*/
+
+	class BaseScope;
+
+	using BaseScopePtr = std::shared_ptr<BaseScope>;
+
+	class BaseScope {
+	public:
+		BaseScope(BaseScopePtr parentScope = nullptr) : parent_(parentScope){
+
+		}
+		BaseScopePtr parent_;
+		std::vector<BaseScopePtr> children_;
+		std::map<std::string, Value*> variableTable_;
+	};
+
+	class Scope {
+	public:
+		Scope() {
+			currentScope_ = std::make_shared<BaseScope>();
+		}
 	private:
-		std::vector<LocalScope> subScopes_;
+		BaseScopePtr currentScope_;
+
+	private:
+		inline void addChild() {
+			auto child = std::make_shared<BaseScope>(currentScope_);
+			currentScope_->children_.push_back(child);
+		}
+		inline void enterNewChild(){
+			size_t size = currentScope_->children_.size();
+			currentScope_ = currentScope_->children_[size - 1];
+		}
+		inline void returnParent() {
+			currentScope_ = currentScope_->parent_;
+			// 删除原来的子作用域
+			currentScope_->children_.pop_back();
+		}
+		inline Value* findVariable(const std::string& name, BaseScopePtr scope) const {
+			auto result = scope->variableTable_.find(name);
+			/*
+			首先在当前scope搜索，无结果转向parent scope继续搜索，
+			均无结果返回nullptr
+			*/
+			if (result != scope->variableTable_.end())
+				return result->second;
+			else if (currentScope_->parent_ != nullptr)
+				return findVariable(name, currentScope_->parent_);
+			else
+				return nullptr;
+		}
+	public:
+		inline void addSubScope() {
+			// 创建并进入子作用域
+			addChild();
+			enterNewChild();
+		}
+		inline void exitScope() {
+			// 返回上一层作用域
+			returnParent();
+		}
+		inline void createVariable(const std::string& name, Value* value) {
+			currentScope_->variableTable_.insert({ name, value });
+		}
+		inline Value* findVariable(const std::string& name) const {
+			return findVariable(name, currentScope_);
+		}
+		inline void setVariable(const std::string& name, Value* value) {
+			currentScope_->variableTable_.find(name)->second = value;
+		}
+		inline void deleteVariable(const std::string& name) {
+			auto ptr = currentScope_->variableTable_.find(name);
+			currentScope_->variableTable_.erase(ptr);
+		}
 	};
 
 	class Machine {
@@ -118,7 +195,6 @@ namespace Guilmon {
 		}
 		inline Value* findVariable(const std::string& name) const {
 			return variableTable_.findVariable(name);
-			
 		}
 		inline void setVariable(const std::string& name, Value* value) {
 			variableTable_.setVariable(name, value);
---
+++ TestGuilmon/TestScope.cpp
@@ -0,0 +1,100 @@
+#include "pch.h"
+#include "Parser.h"
+
+using namespace Guilmon;
+class ScopeTest : public ::testing::Test {
+protected:
+	std::allocator<Value> alloc_;
+	template<typename T> 
+	inline Value* createValue(T value) {
+		auto ptr = alloc_.allocate(sizeof(Value));
+		alloc_.construct(ptr, Value{ value });
+		return ptr;
+	}
+};
+
+TEST_F(ScopeTest, TestCreate) {
+	Scope scope;
+	auto value = createValue(Value{ 10 });
+	scope.createVariable("var", value);
+
+	EXPECT_EQ(scope.findVariable("var")->number, 10);
+}
+
+TEST_F(ScopeTest, TestSet) {
+	Scope scope;
+	auto value = createValue(Value{ 10 });
+	scope.createVariable("var", value);
+	value = createValue(Value{ 25 });
+	scope.setVariable("var", value);
+
+	EXPECT_EQ(scope.findVariable("var")->number, 25);
+}
+
+TEST_F(ScopeTest, TestDelete) {
+	// 单纯删除table内的内容
+	Scope scope;
+	auto value = createValue(Value{ 10 });
+	scope.createVariable("var", value);
+	scope.deleteVariable("var");
+
+	EXPECT_EQ(scope.findVariable("var"), nullptr);
+}
+
+TEST_F(ScopeTest, TestDelete_1) {
+	// 删除子作用域变量，查找上层作用域同名变量
+	Scope scope;
+	auto value = createValue(Value{ 10 });
+	scope.createVariable("var", value);
+
+	scope.addSubScope();
+	value = createValue(Value{ 25 });
+	scope.createVariable("var", value);
+	scope.deleteVariable("var");
+
+	EXPECT_EQ(scope.findVariable("var")->number, 10);
+}
+
+TEST_F(ScopeTest, TestEnterScope) {
+	Scope scope;
+	auto value = createValue(Value{ 10 });
+	scope.createVariable("var", value);
+
+	scope.addSubScope();
+	value = createValue(Value{ 25 });
+	scope.createVariable("var", value);
+
+	EXPECT_EQ(scope.findVariable("var")->number, 25);
+}
+
+TEST_F(ScopeTest, TestExitScope) {
+	Scope scope;
+	auto value = createValue(Value{ 10 });
+	scope.createVariable("var", value);
+
+	scope.addSubScope();
+	value = createValue(Value{ 25 });
+	scope.createVariable("var", value);
+
+	// 返回上层作用域
+	scope.exitScope();
+
+	EXPECT_EQ(scope.findVariable("var")->number, 10);
+}
+
+TEST_F(ScopeTest, TestFind) {
+	Scope scope;
+	auto value = createValue(Value{ 10 });
+	scope.createVariable("var", value);
+
+	scope.addSubScope();
+	value = createValue(Value{ 25 });
+	scope.createVariable("bat", value);
+
+	EXPECT_EQ(scope.findVariable("var")->number, 10);
+}
+
+
+
+
+
